1c1c9f830936f3d7e399fae9df340cec6cc98a63
Maurice Yang <mauriceyangmy28@gmail.com>
2021-10-13 08:55:18 +0700

    Edited rupiah formatting

--- api/templatetags/format.py
+++ api/templatetags/format.py
@@ -1,10 +1,23 @@
-import locale
 from django import template
 
 register = template.Library()
 
 @register.simple_tag
-def rupiah_format(angka, desimal=2):
-    locale.setlocale(locale.LC_NUMERIC, 'IND')
-    rupiah = locale.format("%.*f", (desimal, angka), True)
-    return "Rp. {}".format(rupiah)
+def rupiah_format(value):
+    str_value = str(value)
+    separate_decimal = str_value.split(".")
+    after_decimal = separate_decimal[0]
+    before_decimal = separate_decimal[1]
+
+    reverse = after_decimal[::-1]
+    temp_reverse_value = ""
+
+    for index, val in enumerate(reverse):
+        if (index + 1) % 3 == 0 and index + 1 != len(reverse):
+            temp_reverse_value = temp_reverse_value + val + "."
+        else:
+            temp_reverse_value = temp_reverse_value + val
+
+    temp_result = temp_reverse_value[::-1]
+
+    return "Rp " + temp_result + "," + before_decimal
